733efdff944bb8d2abac1ba702f6429895747553
Daphyne Silva <daphynejosefa@hotmail.com>
2015-10-09 17:05:02 -0300

    Meu primeiro commit Daphyne

---
+++ src/br/com/infox/jdbc/Conexao.java
@@ -0,0 +1,27 @@
+package br.com.infox.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	
+	
+	
+	public static Connection getConnection(){
+		
+		Connection con = null;
+
+		
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bancoinfox","root","");
+			System.out.println("Conectado com sucesso!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Não conseguiu se conectar com o banco."+e.getMessage());
+		}
+		return con;
+		
+	}
+
+}
